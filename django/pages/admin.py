33820b132de1104844208edbe9a49637ff4714f6
Paul Ahern <ahernp@ahernp.com>
2016-09-17 17:31:14 +0100

    Remove extra field from Django admin config.

--- django/pages/admin.py
+++ django/pages/admin.py
@@ -18,7 +18,7 @@ class PageAdmin(admin.ModelAdmin):
             'fields': (('content',),
                        ('title', 'parent',),
                        ('slug',),
-                       ('published', 'updated'),
+                       ('updated'),
             )
         }),
     )

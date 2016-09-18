f674fcafe4d87288df7261387c15206c1622d225
Paul Ahern <ahernp@ahernp.com>
2016-09-18 07:02:03 +0100

    Removed unused code.

--- angular/src/app.routes.ts
+++ angular/src/app.routes.ts
@@ -1,7 +1,6 @@
 import {provideRouter, RouterConfig}  from '@angular/router';
 
 import {PageDetailComponent} from './pages/page-detail.component';
-import {PagesComponent} from './pages/pages.component';
 import {DashboardComponent} from './dashboard.component';
 
 const routes: RouterConfig = [
@@ -13,10 +12,6 @@ const routes: RouterConfig = [
   {
     path: 'page/:slug',
     component: PageDetailComponent
-  },
-  {
-    path: 'pages',
-    component: PagesComponent
   },
   {
     path: 'dashboard',
--- angular/src/pages/page.service.ts
+++ angular/src/pages/page.service.ts
@@ -20,43 +20,11 @@ export class PageService {
             .catch(this.handleError);
     }
 
-    // Add new Page
-    private post(page:Page):Promise<Page> {
-        let headers = new Headers({'Content-Type': 'application/json'});
-
-        return this.http
-            .post(`${appSettings.apiEndpoint}/pages/add/${page.slug}`, JSON.stringify(page), {headers: headers})
-            .toPromise()
-            .then(res => res.json().data)
-            .catch(this.handleError);
-    }
-
-    // Update existing Page
-    private put(page:Page) {
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-
-        let url = `/api/pages/update/${page.id}`;
-
-        return this.http
-            .put(url, JSON.stringify(page), {headers: headers})
-            .toPromise()
-            .then(() => page)
-            .catch(this.handleError);
-    }
-
     getPage(slug:string) {
         return this.http.get(`${appSettings.apiEndpoint}/pages/read/${slug}`)
             .toPromise()
             .then(response => response.json() as Page)
             .catch(this.handleError);
-    }
-
-    save(page:Page):Promise<Page> {
-        if (page.id) {
-            return this.put(page);
-        }
-        return this.post(page);
     }
 
     private handleError(error:any) {

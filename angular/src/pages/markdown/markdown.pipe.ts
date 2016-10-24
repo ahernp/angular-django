a4120f2ae300ae78c10fc7959489b3ed46d8658e
Paul Ahern <ahernp@ahernp.com>
2016-10-24 21:52:26 +0100

    Treat Html from Markdown as safe.

--- angular/src/pages/markdown/markdown.pipe.ts
+++ angular/src/pages/markdown/markdown.pipe.ts
@@ -1,3 +1,4 @@
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 import {Pipe, PipeTransform} from '@angular/core';
 
 import * as showdown from 'showdown';
@@ -6,7 +7,9 @@ var converter = new showdown.Converter({'tables': true});
 
 @Pipe({name: 'markdown'})
 export class MarkdownToHtmlPipe implements PipeTransform {
-    transform(markdown: string): string {
-      return converter.makeHtml(markdown);
+    constructor(private _sanitizer: DomSanitizer){}
+
+    transform(markdown: string): SafeHtml {
+      return this._sanitizer.bypassSecurityTrustHtml(converter.makeHtml(markdown));
     }
 }

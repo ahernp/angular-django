59ec3abe412269c65922f16848c81fba6e1d4298
Paul Ahern <ahernp@ahernp.com>
2016-11-09 09:21:40 +0000

    Enable Production Mode

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -5,5 +5,8 @@ import 'zone.js/dist/zone';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app.module';
 
+import {enableProdMode} from '@angular/core';
+enableProdMode();
+
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);

4654040de2177a39098179c76a9edcdbbdca5874
Paul Ahern <ahernp@ahernp.com>
2016-11-10 17:49:04 +0000

    Allow for Pages not being found.

--- django/pages/views.py
+++ django/pages/views.py
@@ -10,8 +10,11 @@ from pages.models import Page
 def list_pages(request):
     parent_slug = request.GET.get('parent_slug')
     if parent_slug:
-        parent = Page.objects.get(slug=parent_slug)
-        pages = Page.objects.filter(parent=parent)
+        try:
+            parent = Page.objects.get(slug=parent_slug)
+            pages = Page.objects.filter(parent=parent)
+        except Page.DoesNotExist:
+            pages = []
     else:
         pages = Page.objects.all()
     data = json.dumps([page.breadcrumb_dictionary for page in pages])
@@ -19,17 +22,23 @@ def list_pages(request):
 
 
 def read_page(request, slug):
-    parent = Page.objects.get(slug=slug)
-    parent_dictionary = parent.dictionary
-    children = Page.objects.filter(parent=parent).order_by('title')
-    parent_dictionary['children'] = [child.breadcrumb_dictionary for child in children]
-    return HttpResponse(json.dumps(parent_dictionary), content_type='application/json')
+    try:
+        parent = Page.objects.get(slug=slug)
+        data = parent.dictionary
+        children = Page.objects.filter(parent=parent).order_by('title')
+        data['children'] = [child.breadcrumb_dictionary for child in children]
+    except Page.DoesNotExist:
+        data = {'title': 'Page Not Found'}
+    return HttpResponse(json.dumps(data), content_type='application/json')
 
 
 def child_pages(request, parent_slug, limit=3):
-    parent = Page.objects.get(slug=parent_slug)
-    pages = Page.objects.filter(parent=parent).order_by('-published')[:limit]
-    data = json.dumps([page.dictionary for page in pages])
+    try:
+        parent = Page.objects.get(slug=parent_slug)
+        pages = Page.objects.filter(parent=parent).order_by('-published')[:limit]
+        data = json.dumps([page.dictionary for page in pages])
+    except Page.DoesNotExist:
+        data = '[]'
     return HttpResponse(data, content_type='application/json')
 
 

a7b46337e478650f046ea797ff8d2d50b4d01355
Paul Ahern <ahernp@ahernp.com>
2016-11-29 21:42:04 +0000

    Add table utility.

--- angular/src/core/table/table.component.ts
+++ angular/src/core/table/table.component.ts
@@ -1,8 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 
-var filterableStrings: string[];
-var sortColumn: number;
-var sortOrders: boolean[];
+import {Table} from '../../utilities'
 
 @Component({
     selector: 'ad-table',
@@ -10,10 +8,10 @@ var sortOrders: boolean[];
         <input [(ngModel)]="filterString" (ngModelChange)="filterRows()" placeholder="Filter">
         <table>
             <thead>
-                <tr><th *ngFor="let columnHeading of columnHeadings; let i = index" (click)="sortRows(i)">{{columnHeading}}</th></tr>
+                <tr><th *ngFor="let columnHeading of table.columnHeadings; let i = index" (click)="table.sortRows(i)">{{columnHeading}}</th></tr>
             </thead>
             <tbody>
-                <tr *ngFor="let row of currentRows">
+                <tr *ngFor="let row of table.currentRows">
                     <td *ngFor="let column of row">{{column}}</td>
                 </tr>
             </tbody>
@@ -24,55 +22,15 @@ export class TableComponent implements OnInit {
     @Input() columnHeadings: string[];
     @Input() rows: string[][];
 
+    table: Table;
+
     filterString: string;
-    currentRows: string[][];
 
     ngOnInit(): void {
-        this.currentRows = this.rows;
-        this.initialiseSortOrders(this.columnHeadings.length);
-        this.populateFilterStrings();
-    }
-
-    populateFilterStrings(): void {
-        filterableStrings = [];
-        for (let row of this.rows) {
-            filterableStrings.push(row.toString().toLocaleLowerCase());
-        }
+        this.table = new Table(this.columnHeadings, this.rows);
     }
 
     filterRows(): void {
-        if (this.filterString.length < 2) {
-            this.currentRows = this.rows;
-            return;
-        }
-        let filterString = this.filterString.toLocaleLowerCase();
-        this.currentRows = this.rows.filter(
-            (value, index) => filterableStrings[index].indexOf(filterString) != -1);
-    }
-
-    initialiseSortOrders(numberOfHeadings: number): void {
-        sortOrders = [];
-        for (let i = 0; i < numberOfHeadings; i++)
-            sortOrders.push(false);
-    }
-
-    compareRows(rowA: string[], rowB: string[]): number {
-        if (rowA[sortColumn] === rowB[sortColumn])
-            return 0;
-        else
-            if (sortOrders[sortColumn])
-                return (rowA[sortColumn] > rowB[sortColumn]) ? -1 : 1;
-            else
-            return (rowA[sortColumn] < rowB[sortColumn]) ? -1 : 1;
-    }
-
-    sortRows(index: number): void {
-        console.log(index);
-        sortColumn = index;
-        this.rows.sort(this.compareRows);
-        this.populateFilterStrings();
-        if (this.filterString != undefined)
-            this.filterRows();
-        sortOrders[sortColumn] = !sortOrders[sortColumn];
+        this.table.setFilterString(this.filterString);
     }
 }
--- angular/src/utilities.ts
+++ angular/src/utilities.ts
@@ -11,3 +11,78 @@ export function toDateTimeString(date: Date): string {
         leadingZero(date.getUTCMinutes()) + ':' +
         leadingZero(date.getUTCSeconds())
 }
+
+export class Table {
+    public columnHeadings: string[];
+    private rows: string[][];
+    public currentRows: string[][];
+
+    private sortOrders: boolean[] = [];
+    private filterableStrings: string[];
+
+    private sortColumn: number;
+    private filterString: string = '';
+
+    constructor(columnHeadings: string[], rows: string[][]) {
+        this.columnHeadings = columnHeadings;
+        this.rows = rows;
+        this.currentRows = rows;
+        this.initialiseSortOrders(this.columnHeadings.length);
+        this.populateFilterStrings();
+    }
+
+    public setFilterString(filterString: string): void {
+        this.filterString = filterString;
+        this.filterRows();
+    }
+
+    public sortRows(index: number): void {
+        this.sortColumn = index;
+        var self = this;
+        this.rows.sort((rowA: string[], rowB: string[]): number => {
+            if (rowA[self.sortColumn] === rowB[self.sortColumn])
+                return 0;
+            else
+                if (self.sortOrders[self.sortColumn])
+                    return (rowA[self.sortColumn] > rowB[self.sortColumn]) ? -1 : 1;
+                else
+                return (rowA[self.sortColumn] < rowB[self.sortColumn]) ? -1 : 1;
+            })
+        this.populateFilterStrings();
+        if (this.filterString != undefined)
+            this.filterRows();
+        this.sortOrders[this.sortColumn] = !this.sortOrders[this.sortColumn];
+    }
+
+    private initialiseSortOrders(numberOfHeadings: number): void {
+        for (let i = 0; i < numberOfHeadings; i++)
+            this.sortOrders.push(false);
+    }
+
+    private populateFilterStrings(): void {
+        this.filterableStrings = [];
+        for (let row of this.rows)
+            this.filterableStrings.push(row.toString().toLocaleLowerCase());
+    }
+
+    private filterRows(): string[][] {
+        if (this.filterString.length < 2) {
+            this.currentRows = this.rows;
+            return this.currentRows;
+        }
+        let filterStringLowercase = this.filterString.toLocaleLowerCase();
+        this.currentRows = this.rows.filter(
+            (value, index) => this.filterableStrings[index].indexOf(filterStringLowercase) != -1);
+        return this.currentRows;
+    }
+
+    private compareRows(rowA: string[], rowB: string[]): number {
+        if (rowA[this.sortColumn] === rowB[this.sortColumn])
+            return 0;
+        else
+            if (this.sortOrders[this.sortColumn])
+                return (rowA[this.sortColumn] > rowB[this.sortColumn]) ? -1 : 1;
+            else
+            return (rowA[this.sortColumn] < rowB[this.sortColumn]) ? -1 : 1;
+    }
+}

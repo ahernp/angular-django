7b74cbda48a4c81d530fea6a40d588c4d1549e5b
Paul Ahern <ahernp@ahernp.com>
2016-12-04 20:11:42 +0000

    Update image src in lightbox.

--- angular/src/pages/gallerycontent/gallerycontent.component.ts
+++ angular/src/pages/gallerycontent/gallerycontent.component.ts
@@ -17,7 +17,7 @@ import {Gallery, Image} from "./gallery";
                 style="border-radius: 0 3px 3px 0;">&#10094;</span>
             <span (click)="nextImage()" class="lightbox-control lightbox-previous-next"
                 style="right: 0; border-radius: 3px 0 0 3px;">&#10095;</span>
-            <img *ngIf="showImageFlag" src="{{gallery.images[currentIndex].imageUrl}}"
+            <img [src]="gallery.images[currentIndex].imageUrl"
                 style="display: block; margin-left: auto; margin-right: auto; max-height:90%; max-width:90%">
             <p style="text-align: center; color: white;">
                 {{gallery.images[currentIndex].title}}
@@ -35,7 +35,6 @@ export class GalleryContentComponent {
     gallery: Gallery;
     currentIndex: number;
     showLightboxFlag: boolean = false;
-    showImageFlag: boolean = true;
 
     showImage(index: number): void {
         this.currentIndex = index;
@@ -47,20 +46,16 @@ export class GalleryContentComponent {
     }
 
     previousImage(): void {
-        this.showImageFlag = false;
         if (this.currentIndex > 0)
             this.currentIndex--;
         else
             this.currentIndex = this.gallery.images.length - 1;
-        this.showImageFlag = true;
     }
 
     nextImage(): void {
-        this.showImageFlag = false;
         if (this.currentIndex == (this.gallery.images.length - 1))
             this.currentIndex = 0;
         else
             this.currentIndex++;
-        this.showImageFlag = true;
     }
 }

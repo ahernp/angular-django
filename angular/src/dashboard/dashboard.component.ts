f1248d97728f07e542e0b546795a6a8f5f764be0
Paul Ahern <ahernp@ahernp.com>
2016-12-24 17:45:07 +0000

    Add cache counts to Dashboard.

--- angular/src/dashboard/dashboard.component.ts
+++ angular/src/dashboard/dashboard.component.ts
@@ -4,6 +4,9 @@ import {Title} from '@angular/platform-browser';
 import {Dashboard} from './dashboard';
 import {DashboardService} from './dashboard.service';
 
+import {Page} from "../pages/page";
+import {PageService} from "../pages/page.service";
+
 import {Breadcrumb} from "../core/breadcrumbs/breadcrumb";
 import {BreadcrumbService} from "../core/breadcrumbs/breadcrumb.service";
 
@@ -19,28 +22,36 @@ export const dashboardUrl: string = '/dashboard';
     selector: 'ad-dashboard',
     template: `
         <ad-header id="header" *ngIf="breadcrumbs" [breadcrumbs]="breadcrumbs"></ad-header>
-        <div *ngIf="dashboard" id="content">
-                <h1>Status <span style="font-size: small;">at {{dashboard.timeChecked}}</span></h1>
-                <div style="width: 47%; float: left; padding-bottom: 1em; word-wrap: break-word;">
-                    <p *ngIf="dashboard.hostname"><strong>Hostname:</strong> {{dashboard.hostname}}</p>
-                    <h2>Settings Flags</h2>
-                    <span *ngFor="let flag of dashboard.settingsFlags"
-                        [class.highlight]="flag.actual != flag.expected">
-                            {{flag.name}}: {{flag.actual}}&ensp;
+        <div *ngIf="dashboard"id="content">
+            <h1>Status <span style="font-size: small;">at {{dashboard.timeChecked}}</span></h1>
+            <div style="width: 47%; float: left; padding-bottom: 1em; word-wrap: break-word;">
+                <p *ngIf="dashboard.hostname"><strong>Hostname:</strong> {{dashboard.hostname}}</p>
+                <h2>Cache</h2>
+                <table>
+                    <thead><tr><th>Type</th><th>Size</th></tr></thead>
+                    <tbody>
+                        <tr *ngIf="numberOfPages"><td>Database Pages</td><td>{{numberOfPages}}</td></tr>
+                        <tr *ngIf="numberOfDynamicPages"><td>Dynamic Pages</td><td>{{numberOfDynamicPages}}</td></tr>
+                    </tbody>
+                </table>
+                <h2>Settings Flags</h2>
+                <span *ngFor="let flag of dashboard.settingsFlags"
+                    [class.highlight]="flag.actual != flag.expected">
+                        {{flag.name}}: {{flag.actual}}&ensp;
+                </span>
+                <h2>Version Information</h2>
+                <p *ngIf="dashboard.gitVersion"><strong>Most Recent Commit:</strong> {{dashboard.gitVersion}}</p>
+                <p *ngIf="dashboard.pythonPackages"><strong>Python Packages:</strong> {{dashboard.pythonPackages}}</p>
+                <p *ngIf="dashboard.npmPackages"><strong>NPM Packages:</strong> {{dashboard.npmPackages}}</p>
+            </div>
+            <div style="width:47%; float:left; padding-left: 1em;">
+                <h2>Recent Log Entries</h2>
+                <p *ngFor="let entry of dashboard.logEntries">
+                    <span [class.highlight]="entry.level == 'ERROR'">
+                        {{entry.level}}: {{entry.msg}}<br>Logged {{entry.datetime}}
                     </span>
-                    <h2>Version Information</h2>
-                    <p *ngIf="dashboard.gitVersion"><strong>Most Recent Commit:</strong> {{dashboard.gitVersion}}</p>
-                    <p *ngIf="dashboard.pythonPackages"><strong>Python Packages:</strong> {{dashboard.pythonPackages}}</p>
-                    <p *ngIf="dashboard.npmPackages"><strong>NPM Packages:</strong> {{dashboard.npmPackages}}</p>
-                </div>
-                <div style="width:47%; float:left; padding-left: 1em;">
-                    <h2>Recent Log Entries</h2>
-                    <p *ngFor="let entry of dashboard.logEntries">
-                        <span [class.highlight]="entry.level == 'ERROR'">
-                            {{entry.level}}: {{entry.msg}}<br>Logged {{entry.datetime}}
-                        </span>
-                    </p>
-                </div>
+                </p>
+            </div>
             <div style="clear:both"></div>
         </div>
         <ad-footer id="footer" *ngIf="footer" [footer]="footer" (onRefresh)="onRefresh()"></ad-footer>
@@ -51,15 +62,18 @@ export const dashboardUrl: string = '/dashboard';
 export class DashboardComponent implements OnInit {
     now: string;
     dashboard: Dashboard;
+    numberOfPages: number;
+    numberOfDynamicPages: number;
     breadcrumbs: Breadcrumb[];
     footer: Footer;
     showSpinner: Boolean = false;
     error: any;
 
     constructor(
-        private dashboardService:DashboardService,
-        private breadcrumbService:BreadcrumbService,
-        private titleService:Title) {
+        private dashboardService: DashboardService,
+        private breadcrumbService: BreadcrumbService,
+        private pageService: PageService,
+        private titleService: Title) {
     }
 
     ngOnInit(): void {
@@ -68,6 +82,7 @@ export class DashboardComponent implements OnInit {
         this.populateHeader();
         this.populateFooter();
         this.getDashboard();
+        this.getCaches();
     }
 
     populateHeader() {
@@ -97,7 +112,13 @@ export class DashboardComponent implements OnInit {
             });
     }
 
+    getCaches() {
+        this.pageService.getPages()
+            .subscribe(pages => this.numberOfPages = pages.length);
+        this.numberOfDynamicPages = this.pageService.getDynamicPageBreadcrumbs().length;
+    }
+
     onRefresh() {
-        this.getDashboard();
+        this.dashboardService.populateDashboard();
     }
 }
--- angular/src/dashboard/dashboard.service.ts
+++ angular/src/dashboard/dashboard.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 
+import {ReplaySubject} from "rxjs/ReplaySubject";
+
 import {Dashboard} from './dashboard';
 
 import {dashboardUrl} from "./dashboard.component";
@@ -9,11 +11,23 @@ import {apiEndpoint} from "../app.settings";
 @Injectable()
 export class DashboardService {
 
+    dashboardCache: Dashboard;
+    dashboard$: ReplaySubject<any> = new ReplaySubject(1);
+
     constructor(private http:Http) {
+        this.dashboard$.next(new Dashboard());
+        this.populateDashboard();
     }
 
-    getDashboard() {
-        return this.http.get(`${apiEndpoint}${dashboardUrl}/`)
-            .map(response => response.json() as Dashboard);
+    getDashboard(): ReplaySubject<any> {
+        return this.dashboard$;
+    }
+
+    populateDashboard() {
+        this.http.get(`${apiEndpoint}${dashboardUrl}/`)
+            .subscribe(response => {
+                this.dashboardCache = response.json() as Dashboard;
+                this.dashboard$.next(this.dashboardCache);
+            });
     }
 }

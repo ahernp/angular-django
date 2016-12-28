67e05d621947a1ef192df8a7fcf79276cbbb1ea3
Paul Ahern <ahernp@ahernp.com>
2016-12-28 21:11:00 +0000

    Tidied app.settings

--- angular/src/app.settings.ts
+++ angular/src/app.settings.ts
@@ -1,17 +1,20 @@
 import {Breadcrumb} from "./core/breadcrumbs/breadcrumb";
 
-export const adminBreadcrumb = <Breadcrumb>{title: 'Admin', url: '/admin/', externalLinkFlag: true};
+export const adminBreadcrumb = <Breadcrumb>
+    {title: 'Admin', url: '/admin/', externalLinkFlag: true};
 
 export const apiEndpoint: string = '/api';
 
 export const rootSlug: string = 'ahernp-com';
 export const rootTitle: string = 'ahernp.com';
 
+const rootUrl: string = '/';
+export const rootBreadcrumb = <Breadcrumb>
+    {title: rootTitle, url: rootUrl};
+
 /* Dashboard breadcrumb must be defined here to avoid circular imports
    as PageService imports it and PageService is imported by DashboardComponent */
 export const dashboardTitle: string = 'Dashboard';
 export const dashboardUrl: string = '/dashboard';
-export const dashboardBreadcrumb = <Breadcrumb>{title: dashboardTitle, url: dashboardUrl, parentName: rootTitle};
-
-const rootUrl: string = '/';
-export const rootBreadcrumb = <Breadcrumb>{title: rootTitle, url: rootUrl};
+export const dashboardBreadcrumb = <Breadcrumb>
+    {title: dashboardTitle, url: dashboardUrl, parentName: rootTitle};

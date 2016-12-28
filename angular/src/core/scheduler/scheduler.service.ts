d8234251771ab5b9b7f7cdb12e5bf19d8a7a1923
Paul Ahern <ahernp@ahernp.com>
2016-12-28 21:03:22 +0000

    Tidied SchedulerService.

--- angular/src/core/scheduler/scheduler.service.ts
+++ angular/src/core/scheduler/scheduler.service.ts
@@ -5,16 +5,18 @@ const microsecondsPerHour: number = microsecondsPerMinute * 60;
 
 @Injectable()
 export class SchedulerService {
-    hourly(minuteOfHour: number, func: any): void {
+    hourly(minuteOfHour: number, func: Function): void {
         let now: Date = new Date();
         let currentMinute: number = now.getUTCMinutes();
-        let initialTimeout = (currentMinute >= minuteOfHour) ?
-            ((60 - (currentMinute - minuteOfHour)) * microsecondsPerMinute) :
-            ((minuteOfHour - currentMinute) * microsecondsPerMinute)
+        let initialTimeout = (currentMinute >= minuteOfHour)
+            ? ((60 - (currentMinute - minuteOfHour)) * microsecondsPerMinute)
+            : ((minuteOfHour - currentMinute) * microsecondsPerMinute);
 
-        setTimeout(() => {
-            func();
-            setInterval(() => func(), microsecondsPerHour)
-        }, initialTimeout);
+        setTimeout(
+            () => {
+                func();
+                setInterval(() => func(), microsecondsPerHour)
+            },
+            initialTimeout);
     }
 }

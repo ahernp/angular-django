4ec9469b601f1177c4b9a82b4e24df1b0bd4128e
Paul Ahern <ahernp@ahernp.com>
2017-01-28 15:28:37 +0000

    Removed karma

---
+++ angular/webpack.test.config.js
@@ -0,0 +1,17 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/timers/timer.component.spec.ts',
+    output: {
+        path: './dist',
+        filename: 'ad.test.bundle.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.ts$/, loader: 'ts'}
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    }
+};

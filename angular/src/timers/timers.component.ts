a3eaab4010261176dbb6520c1a8bc5acedfc52fc
Paul Ahern <ahernp@ahernp.com>
2017-03-05 08:30:49 +0000

    Remove Festicket timer

--- angular/src/timers/timers.component.ts
+++ angular/src/timers/timers.component.ts
@@ -22,7 +22,6 @@ export const timersBreadcrumb = <Breadcrumb>
             <h2>Paul Ahern</h2>
             <p>Age: <ad-timer targetTimeString="1969-12-05T14:00:00Z"></ad-timer>.
             <p>Time at Affectv: <ad-timer targetTimeString="2016-02-17T10:00:00Z"></ad-timer>.
-            <p>Time since I left Festicket: <ad-timer targetTimeString="2016-02-12T18:00:00Z"></ad-timer>.
             <p>Life expectancy: <ad-timer targetTimeString="2039-12-05T14:00:00Z"></ad-timer>.
             <h2>Nikki Rosenberg</h2>
             <p>Time since last cigarette: <ad-timer targetTimeString="2015-08-04T11:00:00Z"></ad-timer>.
